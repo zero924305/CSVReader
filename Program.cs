37c8fc09b584d4e15dcbed1c386aa1dcf9633faa
Altino <zero924305@gmail.com>
2022-01-26 18:33:27 +0000

    Change method to async/ await method

--- Program.cs
+++ Program.cs
@@ -6,18 +6,21 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace CSVReader
 {
     internal class Program
     {
-        public static string GetDataRecord(List<object> lccs)
+        public static async Task<string> GetDataRecord(List<object> lccs, string queryname)
         {
             List<string> queryData = new();
+            string insertIntoTable = "insert into " + queryname + " values \n";
 
             foreach (System.Dynamic.ExpandoObject ex in lccs)
             {
                 List<string> list = ex.Select(x => x.Value.ToString().Trim()).ToList();
+
                 list = list.Select(x =>
                        x.Replace("\\'", "'")
                         .Replace("’", "'")
@@ -29,13 +32,14 @@ namespace CSVReader
 
                 var concatenatedValues = String.Join(',', list.Select(x => "'" + x + "'"));
 
-                queryData.Add("(" + concatenatedValues + "),\n");
+                queryData.Add("(" + concatenatedValues + ")");
             }
+            insertIntoTable += string.Join(",\n", queryData);
 
-            return string.Join(",\n", queryData); ;
+            return await Task.FromResult(insertIntoTable);
         }
 
-        public static string[] GetHeader(string reader)
+        public static async Task<string[]> GetHeader(string reader)
         {
             string[] header;
             using (StreamReader streamReader = new(reader, Encoding.UTF8))
@@ -45,14 +49,14 @@ namespace CSVReader
                 csvReader.ReadHeader();
                 header = csvReader.HeaderRecord;
             }
-            return header;
+            return await Task.FromResult(header);
         }
 
-        public static string GetSqlHeader(string[] header)
+        public static async Task<string> GetSqlHeader(string[] header, string queryname)
         {
             string queryHeader = "";
-            queryHeader += "DROP TABLE IF EXISTS #temp \n";
-            queryHeader += "CREATE TABLE #temp \n";
+            queryHeader += "DROP TABLE IF EXISTS " + queryname + "\n";
+            queryHeader += "CREATE TABLE " + queryname + "\n";
             queryHeader += "(\n [tempuniqueID] [int] IDENTITY(1,1) NOT NULL,\n";
 
             foreach (string x in header)
@@ -61,37 +65,37 @@ namespace CSVReader
             }
             queryHeader = queryHeader.Remove(queryHeader.Length - 2) + "\n)";
 
-            return queryHeader;
+            return await Task.FromResult(queryHeader);
         }
 
-        public static dynamic GetsqlRecord(string reader)
+        public static async Task<dynamic> GetsqlRecord(string reader)
         {
-            using (StreamReader streamReader = new(reader, Encoding.UTF8))
-            {
-                using (var csvReader = new CsvReader(streamReader, CultureInfo.InvariantCulture))
-                {
-                    var rows = csvReader.GetRecords<dynamic>().ToList();
-                    return rows;
-                }
-            }
+            using StreamReader streamReader = new(reader, Encoding.UTF8);
+            using var csvReader = new CsvReader(streamReader, CultureInfo.InvariantCulture);
+            var rows = csvReader.GetRecords<dynamic>().ToList();
+            return await Task.FromResult(rows);
         }
 
-        private static void Main(string[] args)
+        private static async void Main(string[] args)
         {
+            //file location
             var csv = @"csvlocation";
 
+            //Create a tempTableName
+            string tempTableName = "#" + csv.Trim().Replace(".", "").Replace(" ", string.Empty);
+
             //Get Csv Header
-            string[] header = GetHeader(csv);
-            string queryHeader = GetSqlHeader(header);
+            string[] header = await GetHeader(csv);
+            string queryHeader = await GetSqlHeader(header, tempTableName);
 
             //Get Csv Data
-            List<object> sqlRow = GetsqlRecord(csv);
-            string sqldata = GetDataRecord(sqlRow);
+            List<object> sqlRow = await GetsqlRecord(csv);
+            string sqldata = await GetDataRecord(sqlRow, tempTableName);
 
+            //Full Temp SQL Query
+            string tempSQLQuery = queryHeader + "\n" + sqldata;
             //Output result
-            Console.WriteLine(queryHeader);
-            Console.WriteLine(sqldata);
-            
+            Console.WriteLine(tempSQLQuery);
         }
     }
 }
