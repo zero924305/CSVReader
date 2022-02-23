2edeea32a6b5a36a2cea5c9a3577ed32a4a7eef9
Altino <zero924305@gmail.com>
2022-02-23 12:22:46 +0000

    using stringbuilder in loop function

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace CSVReader
 {
-    internal class Program
+    internal static class Program
     {
 
         public static async Task<string> GetDataRecord(List<object> lccs, string queryname)
@@ -35,27 +35,30 @@ namespace CSVReader
 
                 queryData.Add("(" + concatenatedValues + ")");
             }
-            sqlStatement = await SplitDataRow(queryData, insertIntoTable, sqlStatement, datachunk);
+            sqlStatement = await SplitDataRow(queryData, insertIntoTable, datachunk);
 
             return await Task.FromResult(sqlStatement);
         }
 
-        public static async Task<string> SplitDataRow(List<string> queryData, string insertIntoTable, string sqlStatement, List<string> datachunk)
+        public static async Task<string> SplitDataRow(List<string> queryData, string insertIntoTable,  List<string> datachunk)
         {
+            StringBuilder sqlstatementBuilder = new StringBuilder();
+
             for (int i = 0; i < queryData.Count; i++)
             {
                 //split for each 800 rows
                 if (i % 800 == 0 && i > 0)
                 {
-                    sqlStatement += insertIntoTable + string.Join(",\n", datachunk) + "\n\n";
+                    sqlstatementBuilder.Append(insertIntoTable + string.Join(",\n", datachunk) + "\n\n");
                     datachunk.Clear();
                 }
                 datachunk.Add(queryData.ElementAt(i));
             }
 
             if (datachunk.Count > 0)
-                sqlStatement += insertIntoTable + string.Join(",\n", datachunk);
-            return await Task.FromResult(sqlStatement);
+                sqlstatementBuilder.Append(insertIntoTable + string.Join(",\n", datachunk));
+
+            return await Task.FromResult(sqlstatementBuilder.ToString());
         }
 
         public static async Task<string[]> GetHeader(string reader)
@@ -73,18 +76,15 @@ namespace CSVReader
 
         public static async Task<string> GetSqlHeader(string[] header, string queryname)
         {
-            string queryHeader = "";
-            queryHeader += "DROP TABLE IF EXISTS " + queryname + "\n";
-            queryHeader += "CREATE TABLE " + queryname + "\n";
-            queryHeader += "(\n [tempuniqueID] [int] IDENTITY(1,1) NOT NULL,\n";
+            StringBuilder queryHeadBuilder = new();
+                queryHeadBuilder.Append("DROP TABLE IF EXISTS " + queryname + "\n");
+                queryHeadBuilder.Append("CREATE TABLE " + queryname + "\n");
+                queryHeadBuilder.Append("(\n [tempuniqueID] [int] IDENTITY(1,1) NOT NULL,\n");
 
             foreach (string x in header)
-            {
-                if (x.Length > 128) x.Substring(0, 128);
+                queryHeadBuilder.Append(" [" + (x.Length > 128 ? x.Substring(0, 127) : x) + "] Varchar(MAX),\n");
 
-                queryHeader += " [" + x + "] Varchar(MAX),\n";
-            }
-            queryHeader = queryHeader.Remove(queryHeader.Length - 2) + "\n)";
+            string queryHeader = queryHeadBuilder.ToString().Remove(queryHeadBuilder.Length - 2) + "\n)";
 
             return await Task.FromResult(queryHeader);
         }
@@ -102,7 +102,7 @@ namespace CSVReader
             StringBuilder sb = new();
             foreach (char c in str)
             {
-                if ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || c == '.' || c == '_')
+                if ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
                 {
                     sb.Append(c);
                 }
@@ -126,7 +126,7 @@ namespace CSVReader
                     throw new ArgumentException(@"This file '" + csv + "' is not CSV format");
 
                 //Create a tempTableName
-                var tempTableName = "#" + RemoveSpecialCharacters(csv.Trim().Replace(".", "").Replace(" ", string.Empty));
+                var tempTableName = "#" + RemoveSpecialCharacters(csv.Trim());
 
                 //Get CSV Header
                 var header = await GetHeader(csv);
